2a13ae1883558fa7d0e052fcb3f27b2e3016d99c
mehmet acar <mehmet.acar@udemy.com>
2024-10-12 02:38:07 +0300

    - Add is_produced field to Aircraft

--- manufacturing/signals.py
+++ manufacturing/signals.py
@@ -1,9 +1,13 @@
-# signals.py
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from manufacturing.models import AircraftPart
 
 @receiver(post_save, sender=AircraftPart)
-def update_aircraft_production_status(sender, instance, **kwargs):
-    # Updates the production status of the related aircraft when a new part is added
+def update_aircraft_production_status_on_save(sender, instance, **kwargs):
+    # Updates the production status when a part is added
+    instance.aircraft.check_production_status()
+
+@receiver(post_delete, sender=AircraftPart)
+def update_aircraft_production_status_on_delete(sender, instance, **kwargs):
+    # Updates the production status when a part is removed
     instance.aircraft.check_production_status()
